3e3bf0a324ce4cd7fe3d74c0f9cfbd4098c2a8a7
Chris Birchard <zubazrichards@gmail.com>
2022-01-04 10:20:48 -0700

    Changed hard-coded file name to variable, added todo and documentation.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""Ebay Webscraper
+
+This script will take a list of Lego sets, scrape the Buy It Now prices from Ebay,
+and save the data in a csv for each set
+"""
+
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
@@ -5,7 +11,8 @@ from datetime import date
 from urllib.parse import urlparse
 from search_sets import search_terms
 
-# searchterm = ['21033',
+# below is for testing only
+# searchTerm = ['21033',
 #               '75827'
 #               ]
 
@@ -14,7 +21,11 @@ today = today.strftime("%m-%d-%Y")
 
 
 def get_data(term):
-    url = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw="lego"+"{term}"&_sacat=0&Packaging=Box&_dcat=19006&LH_ItemCondition=1000&rt=nc&LH_BIN=1'
+    """Returns a beautiful soup object from a single page of Ebay Buy It Now items.
+    :term: a Lego set number that is searched on
+    """
+    url = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw="lego"+"{term}"&_sacat=0&Packaging=Box&_dcat=19006' \
+          f'&LH_ItemCondition=1000&rt=nc&LH_BIN=1 '
     r = requests.get(url)
     print(r.status_code)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -22,8 +33,15 @@ def get_data(term):
 
 
 def parse(soup):
+    """Returns a list of sale items for one Lego set number.
+    :soup: a beautiful soup object
+    """
     productslist = []
     results = soup.find_all('div', {'class': 's-item__info clearfix'})
+    # Had a major problem with this div, the first one was usually blank,
+    # so added the first if statement. Should probably use a try except block.
+    # TODO: replace if statement with try except blocks
+    # TODO: add logging functionality
     print(len(results))
     print(type(results))
     print(type(results[0]))
@@ -34,9 +52,9 @@ def parse(soup):
         else:
             price = 'null'
         if item.find('a', {'class': 's-item__link'})['href']:
-            link = item.find('a', {'class': 's-item__link'})['href']
+            link = item.find('a', {'class': 's-item__link'})['href']  # this is the full href
             print(link)
-            parts = urlparse(link)
+            parts = urlparse(link)  # I want the item number in the href, use it as primary key in db
             directories = parts.path.strip('/').split('/')
             item_num = directories[1]
             print(item_num)
@@ -44,7 +62,7 @@ def parse(soup):
             'date': today,
             'setID': term,
             'link': link,
-            'item_num': item_num,
+            'item_num': item_num,  # use as primary key in db to limit repeats
             'price': price
         }
         print(product)
@@ -53,6 +71,11 @@ def parse(soup):
 
 
 def output(productslist, term):
+    """Saves a csv of one set number.
+    :productslist: a list of sale items for one LEgo set number
+    :term: a Lego set number
+    """
+    # TODO: replace writing to csv with writing to a database
     productsdf = pd.DataFrame(productslist)
     productsdf.to_csv(f'LEGO{term}_{today}.csv', index=False)
     print("Saved to csv")
--- search_sets.py
+++ search_sets.py
@@ -1,8 +1,19 @@
+"""Set List Generator
+
+This script reads a csv file that contains set numbers
+and creates a list called search_terms that is used in main
+to iterate over.
+"""
+
 import csv
 
 search_terms = []
-with open('set_test.csv', newline='') as csvfile:
+search_file = 'set_test.csv'
+
+with open(search_file, newline='') as csvfile:
     for row in csv.reader(csvfile):
         search_terms.append(row[0])
 
 print(search_terms)
+
+# TODO: this should probably be a function that returns search_terms[]
