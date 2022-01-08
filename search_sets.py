bf868870c1ae63c1d3ea02e15a4d92884319b85e
Chris Birchard <zubazrichards@gmail.com>
2022-01-08 07:18:39 -0700

    Changed search set to all sets since 2010.

--- search_sets.py
+++ search_sets.py
@@ -2,18 +2,26 @@
 
 This script reads a csv file that contains set numbers
 and creates a list called search_terms that is used in main
-to iterate over.
+to iterate over. Also removes duplicates.
 """
 
-import csv
 
-search_terms = []
-search_file = 'set_test.csv'
+def create_search_list():
+    import csv
 
-with open(search_file, newline='') as csvfile:
-    for row in csv.reader(csvfile):
-        search_terms.append(row[0])
+    search_terms = []
+    search_file = 'All_sets_2021.csv'
 
-print(search_terms)
+    with open(search_file, newline='') as csvfile:
+        for row in csv.reader(csvfile):
+            search_terms.append(row[0])
+
+    # Remove duplicates
+    search_terms = list(dict.fromkeys(search_terms))
+    return search_terms
+
+
+# print(search_terms)
 
 # TODO: this should probably be a function that returns search_terms[]
+# TODO: Go to brickset.com every 3 months and grab a new set of set numbers.
