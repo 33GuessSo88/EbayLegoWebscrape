321d5b77a6517731413dc975550e0a3d678fb7e2
Chris Birchard <zubazrichards@gmail.com>
2022-01-18 06:51:50 -0700

    added an initiation sms

--- send_sms.py
+++ send_sms.py
@@ -1,18 +1,31 @@
-
 from twilio.rest import Client
 
-def send_message(new_records):
 
+def send_inititate_sms():
     account_sid = 'ACe83bcadf007af63af632a4b90a3d5789'
     auth_token = '5e92f2fc80d8fea11a1ed5d3ede8410f'
 
     client = Client(account_sid, auth_token)
 
     message = client.messages \
-                    .create(
-                         body=f"Ebay webscraper successfully ran with {new_records} new records",
-                         from_='+14159039940',
-                         to='+14035401053'
-                     )
+        .create(
+        body=f"Ebay webscraper script initiating now.",
+        from_='+14159039940',
+        to='+14035401053'
+    )
+
+
+def send_finished_sms(new_records):
+    account_sid = 'ACe83bcadf007af63af632a4b90a3d5789'
+    auth_token = '5e92f2fc80d8fea11a1ed5d3ede8410f'
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body=f"Ebay webscraper successfully ran with {new_records} new records",
+        from_='+14159039940',
+        to='+14035401053'
+    )
 
     # print(message.sid)
