086edb104adf41800a2374c623127511b2494937
Chris Birchard <zubazrichards@gmail.com>
2022-03-26 08:00:19 -0600

    combined list generator files into one

--- search_sets.py
+++ search_sets.py
@@ -1,26 +1,40 @@
-"""Set List Generator
-
-This script reads a csv file that contains set numbers
-and creates a list called search_terms that is used in main
-to iterate over. Also removes duplicates.
+"""
+- Creates a connection to the db
+- Extracts the column SetNum where pieces > 1 as a dataframe
+- Removes -1 from set num
+- Creates a list from the SetNum column of df
+- Returns this list called search_terms
 """
 
 
 def create_search_list():
-    import csv
 
-    search_terms = []
-    search_file = 'list_of_all_sets.csv'
+    import pandas as pd
+    import sqlite3
 
-    with open(search_file, newline='') as csvfile:
-        for row in csv.reader(csvfile):
-            search_terms.append(row[0])
+    # create a connection to the database
+    connection = sqlite3.connect('lego.db')
+    cursor = connection.cursor()
+
+    query1 = ''' SELECT DISTINCT SetNum
+            FROM brickset_set_nums
+            WHERE NumPieces > 0
+    '''
+
+    df = pd.read_sql_query(query1, connection)
+
+    connection.close()
+
+    # remove -1 from set_num
+    df['SetNum'] = df['SetNum'].str.split('-', n=1).str[0]
+
+    # create list from df column
+    search_terms = df['SetNum'].tolist()
 
     # Remove duplicates
     search_terms = list(dict.fromkeys(search_terms))
 
-    # print(search_terms)
     return search_terms
 
-
 # TODO: Go to brickset.com every 3 months and grab a new set of set numbers.
+
